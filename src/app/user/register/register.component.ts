8651b76c025cc5a5c6dbd18b244ceb8b0149f7dc
hauze-v <hauze_v@protonmail.com>
2023-02-19 12:07:19 -0700

    added validation to formGroup and reusable InputComponent

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,12 +7,21 @@ import { FormGroup, FormControl } from '@angular/forms';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  /* ------------------------------ Form Controls ----------------------------- */
+  public name = new FormControl('', [Validators.required, Validators.minLength(3)]);
+  public email = new FormControl('', [Validators.required]);
+  public age = new FormControl('', [Validators.required]);
+  public password = new FormControl('', [Validators.required]);
+  public confirm_password = new FormControl('', [Validators.required]);
+  public phoneNumber = new FormControl('', [Validators.required]);
+
+
   public registerForm = new FormGroup({
-    name: new FormControl(''),
-    email: new FormControl(''),
-    age: new FormControl(''),
-    password: new FormControl(''),
-    confirm_password: new FormControl(''),
-    phoneNumber: new FormControl('')
+    name: this.name,
+    email: this.email,
+    age: this.age,
+    password: this.password,
+    confirm_password: this.confirm_password,
+    phoneNumber: this.phoneNumber
   });
 }
