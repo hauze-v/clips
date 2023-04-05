4fb944851c61a9b801fced3307fae1bb6d65ee8d
hauze-v <hauze_v@protonmail.com>
2023-04-05 08:55:04 -0600

    Setup clip data structure

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { last } from 'rxjs';
+import { last, switchMap } from 'rxjs';
 import { v4 as uuid } from 'uuid';
+import firebase from 'firebase/compat/app';
 
 @Component({
   selector: 'app-upload',
@@ -10,6 +12,8 @@ import { v4 as uuid } from 'uuid';
   styleUrls: ['./upload.component.css']
 })
 export class UploadComponent {
+  /* ---------------------------- Public Properties --------------------------- */
+
   /** Keeps track of the hover state for file being dragged */
   public isDragOver: boolean = false;
   public showUploadConfig: boolean = false;
@@ -23,13 +27,16 @@ export class UploadComponent {
   /** Responsible for toggling the percentage in the alert component */
   public showPercentage: boolean = false;
 
+  /** Reference to firebase user object */
+  public user: firebase.User | null = null;
+
   /** Alert Component Properties */
   public showAlert: boolean = false;
   public alertMsg: string = "Please wait! Your clip is being uploaded.";
   public alertColor: string = 'blue';
   public inSubmission: boolean = false;
 
-  /* -------------------------- Form Group & Controls ------------------------- */
+  /** Form Group & Control Properties */
   public title: FormControl = new FormControl('', {
     validators: [
       Validators.required,
@@ -42,9 +49,14 @@ export class UploadComponent {
     videoTitle: this.title,
   })
 
+  /* -------------------------- Constructor Injection ------------------------- */
   constructor(
-    private storageService: AngularFireStorage
-  ) { }
+    private storageService: AngularFireStorage,
+    private fireAuthService: AngularFireAuth,
+  ) {
+    // Subscribe to the user observable from fireAuthService to get the latest user object
+    this.fireAuthService.user.subscribe(user => this.user = user);
+  }
   
   /** 
    * * The file is accessible through the drop's event object
@@ -69,6 +81,7 @@ export class UploadComponent {
 
   
   public uploadFile(): void {
+    // Update alert properties
     this.showAlert = true;
     this.alertMsg = "Please wait! Your clip is being uploaded.";
     this.alertColor = 'blue';
@@ -86,6 +99,12 @@ export class UploadComponent {
 
     // The upload storageService.upload method returns an upload task observable that we can subscribe to for monitoring progress
     const task = this.storageService.upload(clipPath, this.file);
+
+    // The ref function will create a reference to the file in our storage. A reference can be created before a file upload completes
+    // If the file doesn't exist firebase will create a temporary placeholder for you
+    const clipRef = this.storageService.ref(clipPath);
+
+    // Use the percentageChanges() method to get the upload task's progress
     task.percentageChanges().subscribe(progress => {
       this.percentage = progress as number / 100;
     });
@@ -93,14 +112,25 @@ export class UploadComponent {
     // Use the last pipe to get the final (successful state) of the upload task
     // * We use arrow functions here to preserve the 'this' context and object syntax to catch errors
     task.snapshotChanges().pipe(
-      last()
+      last(),
+      switchMap(() => clipRef.getDownloadURL())
     ).subscribe({
-      next: (snapshot) => {
+      next: (url) => {
+        const clip = {
+          uid: this.user?.uid,
+          displayName: this.user?.displayName,
+          title: this.title.value,
+          fileName: `${clipFileName}.mp4`,
+          // ! Create a reference obj that points to a specific obj in our storage. We can use es6's object shorthand syntax here
+          url
+        };
+
+        console.log(clip)
+
         // Update alert component properties
         this.alertMsg = "Success! Your clip is now ready to share with the world.";
         this.alertColor = 'green';
         this.showPercentage = false;
-        // this.inSubmission = false;
       },
       error: (error) => {
         this.alertColor = 'red';
