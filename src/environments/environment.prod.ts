fafac7859700dc3d284d315739cde02dcacea281
hauze-v <hauze_v@protonmail.com>
2023-06-13 12:08:22 -0600

    Removed environments folder from gitignore

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCG1PVQI1GVxBbau5lXlvb97D4NIGGLGcY",
+    authDomain: "clips-b4e9f.firebaseapp.com",
+    projectId: "clips-b4e9f",
+    storageBucket: "clips-b4e9f.appspot.com",
+    messagingSenderId: "702638843921",
+    appId: "1:702638843921:web:ce4896c9a99c84702de9b0"
+  }  
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCG1PVQI1GVxBbau5lXlvb97D4NIGGLGcY",
+    authDomain: "clips-b4e9f.firebaseapp.com",
+    projectId: "clips-b4e9f",
+    storageBucket: "clips-b4e9f.appspot.com",
+    appId: "1:702638843921:web:ce4896c9a99c84702de9b0"
+  }  
+}
